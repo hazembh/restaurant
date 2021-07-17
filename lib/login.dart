7b4acd33ea6d2f3a128c3f5cf6f042bda340c00c
hazembh <73798286+hazembh@users.noreply.github.com>
2021-07-17 22:48:35 +0100

    ..

---
+++ lib/login.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: Colors.black,
+          ),
+          onPressed: () {},
+        ),
+      ),
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}

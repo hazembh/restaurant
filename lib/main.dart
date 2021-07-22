74f370eca943709758c72b9620189db44515cbd0
hazembh <73798286+hazembh@users.noreply.github.com>
2021-07-22 15:35:40 +0100

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application/Home.dart';
+import 'package:flutter_application/signUp.dart';
 
-void main() {
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  //await Firebase.initializedApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  
-  
   @override
   Widget build(BuildContext context) {
-    
-    return MaterialApp( 
-      debugShowCheckedModeBanner:false,
-      home: Home()
-      );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: SignUpPage());
   }
 }
-    
-      
-    
-    
-
-
-  

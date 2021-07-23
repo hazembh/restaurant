db83ea1dddf26f91f73ac09e91a6867c784fe881
hazembh <73798286+hazembh@users.noreply.github.com>
2021-07-23 17:31:19 +0100

    sign in

--- lib/login.dart
+++ lib/login.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application/widget/my_text_field.dart';
 
 class LoginPage extends StatefulWidget {
+  static Pattern pattern =
+      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
   @override
   _LoginPageState createState() => _LoginPageState();
 }
@@ -10,11 +13,122 @@ class LoginPage extends StatefulWidget {
 Color b = Colors.grey[600];
 Color w = Colors.white;
 bool isSwitched = false;
+bool loading = false;
 
 class _LoginPageState extends State<LoginPage> {
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
+  GlobalKey<ScaffoldState> globalKey = GlobalKey<ScaffoldState>();
+  RegExp regExp = RegExp(LoginPage.pattern);
+
+
   @override
+  Future loginUser() async {
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email.text,
+        password: password.text,
+      );
+    } on AuthException catch (e) {
+      if (e.code == 'USER_NOT_FOUND') {
+        globalKey.currentState.showSnackBar(SnackBar(
+            backgroundColor: Colors.grey[200],
+            content: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('No User Found !!',
+                    style: TextStyle(
+                        color: Colors.red, fontWeight: FontWeight.bold))
+              ],
+            )));
+      } else if (e.code == 'WRONG_PASSWORD') {
+        globalKey.currentState.showSnackBar(SnackBar(
+            backgroundColor: Colors.grey[200],
+            content: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Wrong Password !! ',
+                    style: TextStyle(
+                        color: Colors.red, fontWeight: FontWeight.bold))
+              ],
+            )));
+            setState(() {
+              loading=false;
+            });
+      }
+       setState(() {
+              loading=false;
+            });
+    }
+  }
+
+  void validation() {
+    if ((email.text.trim().isEmpty || email.text.trim() == null)&&(password.text.trim().isEmpty || password.text.trim() == null)){
+      globalKey.currentState.showSnackBar(SnackBar(
+          backgroundColor: Colors.grey[200],
+          content: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('All Fields are Empty !',
+                  style:
+                      TextStyle(color: Colors.red, fontWeight: FontWeight.bold))
+            ],
+          )));
+      return;
+
+    }
+    if (email.text.trim().isEmpty || email.text.trim() == null) {
+      globalKey.currentState.showSnackBar(SnackBar(
+          backgroundColor: Colors.grey[200],
+          content: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Please Enter Your Email !',
+                  style:
+                      TextStyle(color: Colors.red, fontWeight: FontWeight.bold))
+            ],
+          )));
+      return;
+    }
+        else if (!regExp.hasMatch(email.text)) {
+      globalKey.currentState.showSnackBar(SnackBar(
+          backgroundColor: Colors.grey[200],
+          content: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Please Enter a valid Email !',
+                  style:
+                      TextStyle(color: Colors.red, fontWeight: FontWeight.bold))
+            ],
+          )));
+    }
+     if (password.text.trim().isEmpty || password.text.trim() == null) {
+      globalKey.currentState.showSnackBar(SnackBar(
+          backgroundColor: Colors.grey[200],
+          content: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Please Enter Your passowrd !',
+                  style:
+                      TextStyle(color: Colors.red, fontWeight: FontWeight.bold))
+            ],
+          )));
+      return;
+    }
+    else {
+      setState(() {
+        loading=true;
+      });
+      loginUser();
+    }
+
+  }
+  
+
+
   Widget build(BuildContext context) {
     return Scaffold(
+      key: globalKey,
       backgroundColor: w,
       body: Container(
         child: SingleChildScrollView(
@@ -65,18 +179,22 @@ class _LoginPageState extends State<LoginPage> {
                     name: 'Email',
                     obscure: false,
                     icon: Icons.email,
-                    controller: null),
+                    controller: email),
                 MyTextField(
                     name: 'Password',
                     obscure: true,
                     icon: Icons.lock,
-                    controller: null),
+                    controller: password),
               ]),
+
               SizedBox(
                 height: 30,
               ),
+              loading? CircularProgressIndicator():
               RaisedButton(
-                onPressed: () {},
+                onPressed: () {
+                  validation();
+                },
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(80.0)),
                 padding: EdgeInsets.all(0.0),
@@ -123,3 +241,4 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 }
+

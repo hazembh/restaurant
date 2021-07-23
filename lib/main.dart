050f14e2748534a9945241e6aeca1949aa98496e
hazembh <73798286+hazembh@users.noreply.github.com>
2021-07-23 17:32:32 +0100

    fire base

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application/Home.dart';
+import 'package:flutter_application/home_page.dart';
+import 'package:flutter_application/login.dart';
 import 'package:flutter_application/signUp.dart';
-
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,6 +15,21 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: SignUpPage());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme:ThemeData(
+        scaffoldBackgroundColor: Color(0xff2b2b2b),
+        appBarTheme: AppBarTheme(color:Color(0xff2b2b2b))
+
+      ),
+      home: StreamBuilder(
+          stream: FirebaseAuth.instance.onAuthStateChanged,
+          builder: (index, sncpshot) {
+            if (sncpshot.hasData) {
+              return HomePage();
+            }
+            return LoginPage();
+          }),
+    );
   }
 }

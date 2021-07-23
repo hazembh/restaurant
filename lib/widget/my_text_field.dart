ffdb5ccfcf4416f0fb7c415f65d780562d582ec7
hazembh <73798286+hazembh@users.noreply.github.com>
2021-07-23 17:33:16 +0100

    d

--- lib/widget/my_text_field.dart
+++ lib/widget/my_text_field.dart
@@ -5,19 +5,22 @@ class MyTextField extends StatelessWidget {
   final bool obscure;
   final IconData icon;
   final TextEditingController controller;
-  MyTextField({@required this.name, @required this.obscure,@required this.icon,this.controller});
+  MyTextField(
+      {@required this.name,
+      @required this.obscure,
+      @required this.icon,
+      this.controller});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-     
       padding: EdgeInsets.all(15),
       child: TextFormField(
-         controller: controller,
-         obscureText:obscure,
+        controller: controller,
+        obscureText: obscure,
         style: TextStyle(color: Colors.black),
         decoration: InputDecoration(
-          prefixIcon: Icon(icon, color: Colors.black),
+            prefixIcon: Icon(icon, color: Colors.black),
             border: OutlineInputBorder(),
             hintText: name,
             hintStyle: TextStyle(color: Colors.black)),
